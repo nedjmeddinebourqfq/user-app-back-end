fd802dd270b083c4167bc7f0f684cb12ba690633
nedjmeddinebourqfq <nedjmobrf@gmail.com>
2024-03-11 00:50:03 +0100

    Your commit message here

--- user/urls.py
+++ user/urls.py
@@ -4,3 +4,6 @@ from .api import userViewset
 router = routers.DefaultRouter()
 router.register('api/user', userViewset ,'users')
 urlpatterns = router.urls
+
+
+#http://localhost:8000/api/auth/register
